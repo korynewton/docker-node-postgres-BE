00c11e68df404d8f4df8b5c10db03fda82cf52e1
Kory Newton <korybnewton@gmail.com>
2020-05-08 09:02:00 -0700

    update knexfile to connect to postgres correctly

--- knexfile.js
+++ knexfile.js
@@ -7,9 +7,13 @@ module.exports = {
       database: process.env.POSTGRES_DB,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
+      host: 'db', //name given to service in docker-compose
     },
     migrations: {
       directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
     },
   },
 };
